9c3f4f9ddc82896c05299030ef950ef66aa3a0f7
ramin711711 <raminhuseynov@hotmail.com>
2024-11-11 17:32:19 +0400

    Add files via upload

---
+++ Anonymous_lambda.py
@@ -0,0 +1,18 @@
+# func = lambda a: a**a
+# print(func(25))
+
+# ndfunc = lambda a,b: a/b
+# print(f"{ndfunc(10,5):.1f}")
+
+# func3 = lambda a: a*a
+# print(func3(5))
+
+# func4 = lambda a: a+a
+# print(func4(800))
+
+# func5 = lambda a,b: str(a+b)
+# print(func5('A','+'))
+
+from math import sqrt
+func6 = lambda a: sqrt(a)
+print(f'{func6(26):.1f}') 
